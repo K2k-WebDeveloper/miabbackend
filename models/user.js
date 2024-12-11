6b2770043436baa9eae15a8873c1a2c81113ad26
K2k-WebDeveloper <itsupport1@k2kitsupport.com>
2024-12-11 15:38:20 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,52 @@
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema(
+//   {
+//     firstName: {
+//       type: String,
+//       required: true,
+//     },
+//     lastName: {
+//       type: String,
+//       required: true,
+//     },
+//     email: {
+//       type: String,
+//       required: true,
+//       unique: true,
+//     },
+//     mobile: {
+//       type: String,
+//       required: true,
+//     },
+//     password: {
+//       type: String,
+//       required: true,
+//     },
+//     blockedUsers: {
+//       type: Boolean,
+//       default: false,
+//     },
+//   },
+//   { timestamps: true }
+// );
+
+// module.exports = mongoose.model("User", userSchema);
+
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    mobile: { type: String, required: true },
+    password: { type: String, required: true },
+    blockedUsers: { type: Boolean, default: false },
+    avatar: { type: String, default: null },
+    isPremium:{ type: Boolean, default: false}
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("User", userSchema);
